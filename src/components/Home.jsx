4941876ca6c792570838928e5314926299d70940
Bhupen <bhupensahoo200127@gmail.com>
2023-09-27 00:08:00 +0530

    footer completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,107 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%, -50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={2000}
+    // showArrows={false}
+    showStatus={false}
+    showThumbs={false}
+  >
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img1} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Watch the future
+      </Heading>
+    </Box>
+
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img2} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Gaming
+      </Heading>
+    </Box>
+
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img3} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img4} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Night Life is Cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p={16}>
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m={'auto'}
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={[40, 400]} filter={'hue-rotate(-130deg)'} />
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={[4, 16]}
+            textAlign={'center'}
+          >
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
+            facilis qui iusto fuga labore eius suscipit alias odio laboriosam
+            itaque, officiis ipsa. Aut natus facilis dolore cupiditate tempore
+            deserunt molestiae. Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Quidem, delectus. In minus placeat consectetur
+            reiciendis voluptate totam fugit hic dolorem, esse necessitatibus
+            atque dicta tempore, molestias quasi temporibus, architecto et?
+            Lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam
+            iste eaque quos aliquid ipsum sit velit! Quas dolore sequi
+            perferendis optio accusamus accusantium soluta dolores error sint!
+            Possimus, ab quaerat! Lorem ipsum dolor sit amet consectetur
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Home;
